d9089194b60071c45f94e0029a4fa7713bb71543
Stanisław Borowy <stabor@startmail.com>
2025-03-19 19:55:02 +0100

    display circle that moves randomly according to perlin noise

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import * as PIXI from 'pixi.js'
+import { Noise } from 'noisejs'
 import './style.css'
 
 const app = new PIXI.Application()
 await app.init({ width: 640, height: 480, background: 0xffffff })
 document.getElementById('simulation-window')?.appendChild(app.canvas)
+
+const startX = Math.random() * 640
+const startY = Math.random() * 480
+const circle = new PIXI.Graphics().circle(startX, startY, 10).fill(0x00ff00)
+app.stage.addChild(circle)
+
+const v = 1
+
+const noise = new Noise(Math.random())
+let noiseOffset = 0
+
+app.ticker.add(ticker => {
+  const angle = noise.perlin2(noiseOffset, noiseOffset) * Math.PI * 2
+  circle.x += Math.cos(angle) * v * ticker.deltaTime
+  circle.y += Math.sin(angle) * v * ticker.deltaTime
+  noiseOffset += 0.01
+})
---
+++ src/types/noisejs.d.ts
@@ -0,0 +1,9 @@
+declare module "noisejs" {
+  export class Noise {
+    constructor(seed?: number);
+    perlin2(x: number, y: number): number;
+    perlin3(x: number, y: number, z: number): number;
+    simplex2(x: number, y: number): number;
+    simplex3(x: number, y: number, z: number): number;
+  }
+}
